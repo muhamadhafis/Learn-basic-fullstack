79b618d2b51d6b4f262b92e3f77cf508ec33c0dc
Muhamad Hafis <hafisgtngm199@gmail.com>
2025-02-03 20:52:40 +0700

    add BOM and DOM folder

--- final-task-learn-js/final-assessment/final-assessment/02-code-style/index.js
+++ final-task-learn-js/final-assessment/final-assessment/02-code-style/index.js
@@ -5,7 +5,7 @@ function getBooks() {
 }
 
 function getBookById(id) {
-  let book = books[id];
+  const book = books[id];
 
   if (!book) {
     return null;
@@ -19,7 +19,7 @@ function saveBook(book) {
 }
 
 saveBook({ id: "book-1", name: "Book 1" });
-let myBooks = getBooks();
+const myBooks = getBooks();
 const myBook = getBookById("book-1");
 
 console.log(myBooks);
